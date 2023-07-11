bdf10451ec6d92b3d8e3d9666a057b622478f36e
Springles <104740481+Springlesss@users.noreply.github.com>
2023-07-11 18:55:13 +0200

    Delete mcstatusschema.js

--- commands/mcstatusschema.js
+++
@@ -1,9 +0,0 @@
-const {model, Schema} = require('mongoose'); //importing from mongoose
- 
-let mcSchema = new Schema({ //creating a new schema
-    ip: String, //option ip as string
-    UserID: String, //option userid as string
-    Guild: String //option guild as string
-});
- 
-module.exports = model("mcSchema", mcSchema); //exporting the schema so we can use it in any file we want
